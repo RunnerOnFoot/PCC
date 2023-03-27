4acc3e6e01ec548070c27048fbf72c16e7f3ab77
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-03-27 15:09:00 +0430

    Keep Going

---
+++ Chapter_4/Working with Part of a List/players(6).py
@@ -0,0 +1,5 @@
+# You can include a third value in the brackets indicating a slice.
+# If a third value is included, this tells Python how many items to skip between items in the specified range.
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:4:2])
