3d70ac286418c821ac3645603a8f8c22819b91a5
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-04-16 02:14:52 +0430

    Go One More!

---
+++ Chapter_4/Exercises/buffet.py
@@ -0,0 +1,10 @@
+foods = ("pasta", "pizza", "chicken", "sandwich", "salad")
+for food in foods:
+    print(food.title())
+
+# foods[1] = "roast beef"
+print("\n")
+
+modified_foods = ("pasta", "hot dog", "kebab", "sandwich", "salad")
+for food in modified_foods:
+    print(food.title())
