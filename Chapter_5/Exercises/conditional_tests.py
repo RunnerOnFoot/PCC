a005193d42fff705d1205b8587fce6fcbdde2190
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-04-21 01:10:03 +0430

    i wrote 3 examples for conditional tests.

---
+++ Chapter_5/Exercises/conditional_tests.py
@@ -0,0 +1,32 @@
+# exp.1
+car = "bmw"
+print("Is car == 'bmw'? I predict True.")
+print(car == "bmw")
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == "ford")
+
+# exp.2
+fruit = "apple"
+print("\nIs fruit == 'banana'? I predict False.")
+print(fruit == "banana")
+
+print("\nIs fruit == 'apple'? I predict True.")
+print(fruit == "apple")
+
+print("\nIs fruit == 'mango'? I predict False.")
+print(fruit == "mango")
+
+print("\nIs fruit == 'lemon'? I predict False.")
+print(fruit == "lemon")
+
+# exp.3
+sport = "running"
+print("\nIs sport == 'wrestling'? I predict False.")
+print(sport == "wrestling")
+
+print("\nIs sport == 'climbing'? I predict False.")
+print(sport == "climbing")
+
+print("\nIs sport == 'running'? I predict True.")
+print(sport == "running")
