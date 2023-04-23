f23b392f8cb1112148a8684e5ea442aae7b37348
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-04-24 01:07:01 +0430

    working on toppings :)

---
+++ Chapter_5/Using if Statements with Lists/toppings_2.py
@@ -0,0 +1,10 @@
+# let’s check whether the list of requested toppings is empty before building the pizza.
+
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_topping}.")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
