8988ade358f7ed42d0856c7e6d29472752d641f6
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-04-26 21:16:26 +0330

    some exercises

---
+++ Chapter_6/Exercises/favorite_numbers.py
@@ -0,0 +1,12 @@
+# 6-2
+
+favorite_numbers = {
+    "amin": 5,
+    "ali": 7,
+    "pantea": 10,
+    "bardia": 50,
+    "parnian": 18,
+}
+
+for k, v in favorite_numbers.items():
+    print(f"{k.title()}'s favorite number: {v}.")
---
+++ Chapter_6/Exercises/person.py
@@ -0,0 +1,17 @@
+# 6-1
+
+my_friend = {
+    "first_name": "masy",
+    "last_name": "naji",
+    "age": 24,
+    "city": "rasht",
+}
+
+for k, v in my_friend.items():
+    print(k, v)
+
+print("\n")
+
+# another way:
+for v in my_friend.values():
+    print(v)
