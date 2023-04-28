f789c4988ddca4c0b87b817baa37dba2acc0f8c9
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-04-29 01:30:45 +0330

    Looping Through All the Keys in a Dictionary

---
+++ Chapter_6/Looping Through a Dictionary/favorite_languages_4.py
@@ -0,0 +1,20 @@
+# We’ll loop through the names in the dictionary as we did previously,
+# but when the name matches one of our friends,
+# we’ll display a message about their favorite language:
+favorite_languages = {
+    "jen": "python",
+    "sarah": "c",
+    "edward": "rust",
+    "phil": "python",
+}
+
+friends = ["phil", "sarah"]
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}.")
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+
+# "\t" is a tab!
