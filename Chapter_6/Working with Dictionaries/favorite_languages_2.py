2b7ba9d3674d723af172023659158653482ca0c1
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-04-29 01:32:06 +0330

    Looping Through All Key-Value Pairs

--- Chapter_6/Working with Dictionaries/favorite_languages_2.py
+++
@@ -1,9 +0,0 @@
-favorite_languages = {
-    "jen": "python",
-    "sarah": "c",
-    "edward": "rust",
-    "phil": "python",
-}
-
-for k, v in favorite_languages.items():
-    print(f"{k.title()}'s favorite language is {v.title()}.")
