7485d6b9a61cae1492df5952573b2297e15ae220
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-04-28 19:15:50 +0330

    modified with another way

--- Chapter_6/Exercises/favorite_numbers.py
+++ Chapter_6/Exercises/favorite_numbers.py
@@ -10,3 +10,21 @@ favorite_numbers = {
 
 for k, v in favorite_numbers.items():
     print(f"{k.title()}'s favorite number: {v}")
+
+# another way:
+print("\n")
+
+num = favorite_numbers["amin"]
+print(f"Amin's favorite number is {num}.")
+
+num = favorite_numbers["ali"]
+print(f"Ali's favorite number is {num}.")
+
+num = favorite_numbers["pantea"]
+print(f"Pantea's favorite number is {num}.")
+
+num = favorite_numbers["bardia"]
+print(f"Bardia's favorite number is {num}.")
+
+num = favorite_numbers["parnian"]
+print(f"Parnian's favorite number is {num}.")
