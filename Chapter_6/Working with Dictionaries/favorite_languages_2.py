ae16ee0a11eed33987339901985ac23b85320fde
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-04-29 00:47:52 +0330

    modified this file with method item().

---
+++ Chapter_6/Working with Dictionaries/favorite_languages_2.py
@@ -0,0 +1,9 @@
+favorite_languages = {
+    "jen": "python",
+    "sarah": "c",
+    "edward": "rust",
+    "phil": "python",
+}
+
+for k, v in favorite_languages.items():
+    print(f"{k.title()}'s favorite language is {v.title()}.")
