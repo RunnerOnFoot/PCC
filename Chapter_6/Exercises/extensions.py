64e4fca68e4e06a7f01d708daf90649e15f45818
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-07 16:51:11 +0330

    Not yet completed

---
+++ Chapter_6/Exercises/extensions.py
@@ -0,0 +1,39 @@
+# 6-12
+cities = {
+    'moscow': {
+        'country': 'Russia',
+        'population': 11_980_000,
+        'closest beach': 'Bolshoi Gorodskoi Prud',
+        'weather': '5.7 °C',
+        'elevation': '156m',
+        'area': '2,511 km²',
+        'founded': 1147,
+    },
+    'london': {
+        'country': 'England',
+        'population': 8_982_000,
+        'closest beach': 'Ruislip Lido',
+        'weather': '10.8 °C',
+        'elevation': '11m',
+        'area': '1,572 km²',
+        'founded': '47 AD',
+    },
+    'istanbul': {
+        'country': 'Turkey',
+        'population': 15_460_000,
+        'closest beach': 'Caddebostan',
+        'weather': '14.9 °C',
+        'elevation': '40m',
+        'area': '5,461 km²',
+        'founded': '330 AD',
+    },
+    'dubai': {
+        'country': 'United Arab Emirates',
+        'population': 3_331_000,
+        'closest beach': 'Jumeirah',
+        'weather': '30 ⁰C',
+        'elevation': '5m',
+        'area': '35 km²',
+        'founded': 1833,
+    },
+}
