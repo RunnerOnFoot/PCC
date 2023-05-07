785775a5087aa91eec01f783d6b1e6b8ee5776ab
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-07 16:54:05 +0330

    exercise 7-2

---
+++ Chapter_7/Exercises/restaurant_seating.py
@@ -0,0 +1,8 @@
+# 7-2
+numbers = input("How many people are in your dinner group? ")
+numbers = int(numbers)
+
+if numbers > 8:
+    print("You'll have to wait for a table.")
+else:
+    print("Your table is ready :D")
