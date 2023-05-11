3a01e6a607fadbd2ee10c34148093123b84f2d93
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-11 17:02:24 +0330

    exercise 7-6

---
+++ Chapter_7/Exercises/three_exits.py
@@ -0,0 +1,20 @@
+# 7-6
+PROMPT = "\nHow old are you? "
+PROMPT += "\nEnter 'quit' when you are finished: "
+
+ACTIVE = True
+
+while ACTIVE:
+    age = input(PROMPT)
+
+    if age == 'quit':
+        ACTIVE = False
+        break
+
+    age = int(age)
+    if age < 3:
+        print("The ticket is free :D")
+    elif age <= 12:
+        print("The ticket is $10")
+    elif age > 12:
+        print("The ticket is $15")
