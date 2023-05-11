b1c2ffd3df5ef54f04484245afe5c91a592361d4
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-11 18:15:20 +0330

    FILLING A DICTIONARY WITH USER INPUT

---
+++ Chapter_7/Using a while Loop with Lists and Dictionaries/mountain_poll.py
@@ -0,0 +1,22 @@
+# FILLING A DICTIONARY WITH USER INPUT #
+responses = {}
+# Set a flag to indicate that polling is active.
+POLLING_ACTIVE = True
+
+while POLLING_ACTIVE:
+    # Prompt for the person's name and response.
+    name = input("\nWhat is your name? ")
+    response = input("\nWhich mountain would you like to climb someday? ")
+
+    # Store the response in the dictionary.
+    responses[name] = response
+
+    # Find out if anyone else is going to take the poll.
+    repeat = input("Would you like to let another person respond? (yes/ no) ")
+    if repeat == 'no':
+        POLLING_ACTIVE = False
+
+# Polling is complete. Show results.
+print("\n--- Poll Results ---")
+for name, response in responses.items():
+    print(f"{name.title()} would like to climb {response.title()}.")
