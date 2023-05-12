8c060527aeca67bdd3f31cc525c0343edabf7c35
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-12 17:12:01 +0330

    Passing Information to a Function

---
+++ Chapter_8/Defining a Function/greeter_1_passing_information.py
@@ -0,0 +1,12 @@
+"""Passing Information to a Function"""
+
+
+def greet_user(username):
+    """Display a simple greeting."""
+    print(f"Hello {username.title()}!")
+
+
+greet_user('brian')
+greet_user('sarah')
+greet_user('alex')
+greet_user('jesse')
