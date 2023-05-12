68de094bef3e792df7cb9d31a7e5aaf36ff31e50
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-12 18:55:02 +0330

    Exercise 8-2

---
+++ Chapter_8/Exercises/favorite_book.py
@@ -0,0 +1,10 @@
+"""Exercise 8-2"""
+
+
+def favorite_book(title):
+    """Display a message about the book."""
+    print(f"One of my favorite books is {title}.")
+
+
+favorite_book('Python Crash Course')
+favorite_book('Brida')
