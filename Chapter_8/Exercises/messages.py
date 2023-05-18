41e9646c4ee7349a760af60d08de83dcd6ea3fb3
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-18 20:36:18 +0330

    Exercise 8-9

---
+++ Chapter_8/Exercises/messages.py
@@ -0,0 +1,11 @@
+"""Exercise 8-9"""
+
+
+def show_messages(messages):
+    """Display messages through a list."""
+    for message in messages:
+        print(message)
+
+
+texts = ["Hi I'm Bardia.", "Wassup nigga?", "today is today."]
+show_messages(texts)
