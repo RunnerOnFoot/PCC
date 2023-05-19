f45fb0b2b679f85b36db2564bab52a1d724db6c2
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-20 01:33:18 +0330

    Replace the print() call with a loop - modified the first_version

---
+++ Chapter_8/Passing an Arbitrary Number of Arguments/pizza_1_loop.py
@@ -0,0 +1,12 @@
+""""Replace the print() call with a loop"""
+
+
+def make_pizza(*toppings):
+    """Summarize the pizza we are about to make."""
+    print("\nMaking a pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
