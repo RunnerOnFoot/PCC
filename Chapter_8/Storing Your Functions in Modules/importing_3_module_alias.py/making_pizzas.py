9b33e7eb6cde4287c1b6b21d35e628bda61ee708
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-22 11:45:55 +0330

    Using as to Give a Module an Alias

---
+++ Chapter_8/Storing Your Functions in Modules/importing_3_module_alias.py/making_pizzas.py
@@ -0,0 +1,12 @@
+"""Using as to Give a Module an Alias"""
+
+
+import pizza as p
+
+
+p.make_pizza(16, 'pepperoni')
+p.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+
+# The general syntax for this approach is:
+# import module_name as mn
