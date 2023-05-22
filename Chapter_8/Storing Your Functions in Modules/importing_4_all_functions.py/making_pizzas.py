f875bd9cc2213b9cf9607a37db749b7ddac35903
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-22 12:24:20 +0330

    Importing All Functions in a Module (*)

---
+++ Chapter_8/Storing Your Functions in Modules/importing_4_all_functions.py/making_pizzas.py
@@ -0,0 +1,10 @@
+"""Importing All Functions in a Module"""
+
+
+from pizza import *
+
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+# from module_name import *
