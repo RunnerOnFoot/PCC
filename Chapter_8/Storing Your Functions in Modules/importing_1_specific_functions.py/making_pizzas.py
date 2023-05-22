23027f776f7165b05592d6f6942deb727e8e0341
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-22 10:56:30 +0330

    importing specific functions

---
+++ Chapter_8/Storing Your Functions in Modules/importing_1_specific_functions.py/making_pizzas.py
@@ -0,0 +1,12 @@
+"""Importing Specific Functions"""
+
+
+from pizza import make_pizza
+
+
+make_pizza(16, 'pepperoni')
+make_pizza(16, 'mushrooms', 'green peppers', 'extra cheese')
+
+# You can import as many functions as you want from a module
+# by separating each function’s name with a comma:
+# from module_name import function_0, function_1, function_2
