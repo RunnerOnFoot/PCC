80e7ee93be3f53b618285afd18beecefbccb0f45
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-22 13:27:01 +0330

    Exercise 8-16

---
+++ Chapter_8/Exercises/imports 8-16/imports.py
@@ -0,0 +1,20 @@
+"""Exercise 8-16"""
+
+
+from shirt import *
+import shirt as sh
+from shirt import make_shirt as ms
+from shirt import make_shirt
+import shirt
+shirt.make_shirt(size='xxl', text='Who Knows?')
+
+
+make_shirt(size='xxl', text='Who Knows?')
+
+
+ms(size='xxl', text='Who Knows?')
+
+
+sh.make_shirt(size='xxl', text='Who Knows?')
+
+make_shirt(size='xxl', text='Who Knows?')
