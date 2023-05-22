976f1364b27e5c61981dc92df988b4e6d8e697e0
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-22 13:26:22 +0330

    Functions related to imports.py

---
+++ Chapter_8/Exercises/imports 8-16/shirt.py
@@ -0,0 +1,7 @@
+"""Functions related to imports.py"""
+
+
+def make_shirt(text='I love Python', size='L'):
+    """Summarize the shirt that's going to be made."""
+    print(f"\nI'm going to make a {size} t-shirt.")
+    print(f'It will say, "{text}"')
