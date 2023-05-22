3e9b875c7751ccaa84f37d78ac98b30fcb1a70d3
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-22 13:04:00 +0330

    Exercise 8-15

---
+++ Chapter_8/Exercises/printing_models.py
@@ -0,0 +1,9 @@
+"""Exercise 8-15"""
+
+import printing_functions as pf
+
+designs = ['iphone case', 'robot pendant', 'dodecahedron']
+all_models = []
+
+pf.print_models(designs, all_models)
+pf.show_completed_models(all_models)
