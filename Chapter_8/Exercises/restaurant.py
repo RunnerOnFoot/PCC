44c4f391172ace4835a964381276306955ac6ebb
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-24 15:07:09 +0330

    Exercise 9-1

---
+++ Chapter_8/Exercises/restaurant.py
@@ -0,0 +1,29 @@
+"""Exercise 9-1"""
+
+
+class Restaurant:
+    """A class representing a restaurant."""
+
+    def __init__(self, name, cuisine_type) -> None:
+        """Initialize restaurant name and cuisine."""
+
+        self.name = name.title()
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        """Display a summary of the restaurant."""
+        msg = f"{self.name} serves wonderful {self.cuisine_type}."
+        print(f"\n{msg}")
+
+    def open_restaurant(self):
+        """Display a message that the restaurant is open."""
+        msg = f"{self.name} is open. Come on in!"
+        print(f"\n{msg}")
+
+
+restaurant = Restaurant('Bob Bob Ricard Soho', 'Beef Wellington')
+print(restaurant.name)
+print(restaurant.cuisine_type)
+
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
