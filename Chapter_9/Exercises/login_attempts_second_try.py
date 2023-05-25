1553b0846af60c0d076c419f5e5add34dd6f8a82
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-25 11:50:53 +0330

    Exercise 9-5

---
+++ Chapter_9/Exercises/login_attempts_second_try.py
@@ -0,0 +1,53 @@
+"""Exercise 9-5 (second try)"""
+
+
+class User:
+    """A class representing a user."""
+
+    def __init__(self, first_name, last_name, age,
+                 job, favorite_color, location) -> None:
+        """Initialize the user."""
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.age = age
+        self.job = job.title()
+        self.favorite_color = favorite_color.title()
+        self.location = location.title()
+        self.login_attempts = 0
+
+    def describe_user(self):
+        """Display a summary of the user's information."""
+        print(f"\n{self.first_name} {self.last_name}")
+        print(f"  Job: {self.job}")
+        print(f"  Favorite color: {self.favorite_color}")
+        print(f"  Location: {self.location}")
+
+    def greet_user(self):
+        """Display personalized greeting to the user."""
+        fullname = f"{self.first_name} {self.last_name}"
+        print(f"\nHello {fullname} :D")
+
+    def increment_login_attempts(self):
+        """Increments the value of login_attempts by 1."""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Resets the value of login_attempts to 0."""
+        self.login_attempts = 0
+
+
+parsa = User('parsa', 'moradi', '30', 'programmer', 'orange', 'alaska')
+parsa.describe_user()
+parsa.greet_user()
+
+print("\nMaking 5 login attempts...")
+parsa.increment_login_attempts()
+parsa.increment_login_attempts()
+parsa.increment_login_attempts()
+parsa.increment_login_attempts()
+parsa.increment_login_attempts()
+print(f"  Login Attempts: {parsa.login_attempts}")
+
+print("\nResetting login attempts...")
+parsa.reset_login_attempts()
+print(f"  Login Attempts: {parsa.login_attempts}")
