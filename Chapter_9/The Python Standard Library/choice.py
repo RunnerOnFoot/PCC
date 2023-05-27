450af6b4d936281c90902d885f3193dfcf692ab6
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-27 14:56:29 +0330

    Trying to figure out what 'Python Standard Library' is

---
+++ Chapter_9/The Python Standard Library/choice.py
@@ -0,0 +1,9 @@
+"""Still trying to figure it out :D"""
+
+# choice(): This function takes in a list or tuple
+# and returns a randomly chosen element.
+from random import choice
+
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+first_up = choice(players)
+print(first_up)
---
+++ Chapter_9/The Python Standard Library/random_number.py
@@ -0,0 +1,8 @@
+"""Trying to figure out what 'Python Standard Library' is."""
+
+# randint(): This function takes two integer arguments
+# and returns a randomly selected integer between (and including) those numbers.
+from random import randint
+
+random_number = randint(1, 6)
+print(random_number)
