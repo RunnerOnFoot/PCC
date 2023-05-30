335383018c0da1ab42e48445649af1082de9a83c
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-30 13:59:28 +0330

    Working with a File's Contents

---
+++ Chapter_10/Reading from a File/pi_string_0_first_version.py
@@ -0,0 +1,15 @@
+"""Working with a File's Contents"""
+
+from pathlib import Path
+
+path = Path(
+    'c:/Users/parsa/VSCode/PCC/Chapter_10/Reading from a File/pi_digits.txt')
+contents = path.read_text()
+
+lines = contents.splitlines()
+pi_string = ''
+for line in lines:
+    pi_string += line
+
+print(pi_string)
+print(len(pi_string))
