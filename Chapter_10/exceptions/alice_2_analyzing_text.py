bb02c357b007f2882d235a5a22ef377430f0d92b
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-05-31 20:32:32 +0330

    Analyzing Text

---
+++ Chapter_10/exceptions/alice_2_analyzing_text.py
@@ -0,0 +1,14 @@
+"""Analyzing Text"""
+
+from pathlib import Path
+
+path = Path(r'Chapter_10\exceptions\alice.txt')
+try:
+    contents = path.read_text(encoding='utf-8')
+except FileNotFoundError:
+    print(f"Sorry, the file {path} does not exist.")
+else:
+    # Count the approximate number of the words in the file:
+    words = contents.split()
+    num_words = len(words)
+    print(f"The file {path} has about {num_words} words.")
