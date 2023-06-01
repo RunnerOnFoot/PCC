9037dc65be683340fdffd05d97c6efaa9218efa6
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-06-01 10:34:33 +0330

    Training for myself to fully understand

---
+++ Chapter_10/exceptions/word_count_0_retrain.py
@@ -0,0 +1,20 @@
+"""Working with Multiple Files (retrain)"""
+
+from pathlib import Path
+
+
+def count_words(path):
+    """Count the approximate numbers of words in a file """
+    try:
+        contents = path.read_text(encoding='utf-8')
+    except FileNotFoundError:
+        print(f"Sorry, the file {path} does not exist.")
+    else:
+        # Count the approximate numbers of words in a file:
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file {path} has about {num_words} words.")
+
+
+alice_book = Path(r'Chapter_10\exceptions\alice.txt')
+count_words(alice_book)
