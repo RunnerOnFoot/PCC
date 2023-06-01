a977681d6a2a56d8fe0633d0907c5bb4d937363e
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-06-01 14:03:21 +0330

    Quick understanding of count() method

---
+++ Chapter_10/Exercises/count_method.py
@@ -0,0 +1,10 @@
+"""
+Use the count() method to find out how many times a word or phrase appears in a string.
+For example, the following code counts the number of times 'row' appears
+in a string.
+"""
+
+LINE = "Row, row, row your boat"
+print(LINE.count('row'))
+
+print(LINE.lower().count('row'))
