b53c01be9c66f92b89f34bfec8d80d6b6638bf6c
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-06-01 12:03:11 +0330

    Exercise 10-6 (eric's way) but my way is more beautiful ;)

---
+++ Chapter_10/Exercises/addition_erics_way.py
@@ -0,0 +1,13 @@
+"""Exercise 10-6 (eric's way)"""
+
+try:
+    x = input("Give me a number: ")
+    x = int(x)
+
+    y = input("Give me another number: ")
+    y = int(y)
+except ValueError:
+    print("Sorry, I really needed a number.")
+else:
+    sum = x + y
+    print(f"The sum of {x} and {y} is {sum}.")
