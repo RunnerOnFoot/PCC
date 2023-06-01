2dae8e9f65eb8b8f6c2132d42528aa91b15f7350
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-06-01 18:18:57 +0330

    Saving and Reading User-Generated Data

---
+++ Chapter_10/storing_data/remember_me.py
@@ -0,0 +1,15 @@
+"""Saving and Reading User-Generated Data"""
+
+# Let’s look at an example where we prompt the user for
+# their name the first time they run a program and then remember their
+# name when they run the program again.
+from pathlib import Path
+import json
+
+username = input("What is your name? ")
+
+path = Path(r'Chapter_10\storing_data\username.json')
+contents = json.dumps(username)
+path.write_text(contents, encoding='utf-8')
+
+print(f"We'll remember you when you come back, {username}!")
