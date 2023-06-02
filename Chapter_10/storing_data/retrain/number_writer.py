d855403807a0a9da724cedf61fb6c8acb2a71036
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-06-02 14:19:10 +0330

    Using json.dumps()

---
+++ Chapter_10/storing_data/retrain/number_writer.py
@@ -0,0 +1,10 @@
+"""Using json.dumps()"""
+
+from pathlib import Path
+import json
+
+numbers = [2, 3, 5, 7, 11, 13]
+
+path = Path(r'Chapter_10\storing_data\retrain\numbers.json')
+contents = json.dumps(numbers)
+path.write_text(contents, encoding='utf-8')
