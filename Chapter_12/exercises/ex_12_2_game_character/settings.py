b2d189bf4658168301c240cda7868f883d3517ff
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-07-16 19:12:26 +0330

    Changing the screen height

--- Chapter_12/exercises/ex_12_2_game_character/settings.py
+++ Chapter_12/exercises/ex_12_2_game_character/settings.py
@@ -13,5 +13,5 @@ class Settings:
         """Initialize the game's settings."""
         # Screen settings
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 780
         self.bg_color = (0, 0, 0)
