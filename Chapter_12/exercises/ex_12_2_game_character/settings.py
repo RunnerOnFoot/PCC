660d3409f9260fb77916fdc2707d0d9d83d8e876
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-07-16 17:28:53 +0330

    Settings for Blue Bird game

---
+++ Chapter_12/exercises/ex_12_2_game_character/settings.py
@@ -0,0 +1,17 @@
+"""
+Settings for Blue Bird Game
+Exercise 12-2
+"""
+
+import pygame
+
+
+class Settings:
+    """A class to store all settings for Blue Bird Game."""
+
+    def __init__(self) -> None:
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 0, 0)
