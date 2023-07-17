ac54edb966e09f42c89a1dcad14c390c785ccc54
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-07-17 20:42:18 +0330

    Adding some comments and limiting the frame rate

--- Chapter_12/exercises/ex_12_2_game_character/blue_bird_game.py
+++ Chapter_12/exercises/ex_12_2_game_character/blue_bird_game.py
@@ -25,16 +25,21 @@ class BlueBirdGame:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
+            # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
             self.bird.blitme()
 
+            # Make the most recently drawn screen visible.
             pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == '__main__':
+    # Make a game instance, and run the game.
     bbg = BlueBirdGame()
     bbg.run_game()
