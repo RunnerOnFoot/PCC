baffbaecffc018152769b137510ba5e215abd955
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-10-18 21:03:00 +0330

    Fixed the extra indentation for (def draw_bullet)

--- Chapter_12/exercises/ex_12_6_sideways_shooter/bullet.py
+++ Chapter_12/exercises/ex_12_6_sideways_shooter/bullet.py
@@ -29,6 +29,6 @@ class Bullet(Sprite):
         # Update the rect position.
         self.rect.x = self.x
 
-        def draw_bullet(self):
-            """Draw the bullet to the screen."""
-            pygame.draw.rect(self.screen, self.color, self.rect)
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
