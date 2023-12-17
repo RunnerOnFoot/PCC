5dc45916a3a7d677ffd2dc6773941e26f08dffd8
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-12-17 21:18:04 +0330

    Defining the Pizza Model

--- Chapter_18/exercises/ex_18_4_pizzeria/pizzas/models.py
+++ Chapter_18/exercises/ex_18_4_pizzeria/pizzas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Pizza(models.Model):
+    """Represent a pizza that can be ordered from the pizzeria."""
+    name = models.CharField(max_length=255)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the name of the pizza."""
+        return self.name
