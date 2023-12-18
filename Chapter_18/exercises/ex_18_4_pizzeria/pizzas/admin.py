e05eec38eb7a49e65d7c2c6802bff564e715657f
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-12-18 10:33:40 +0330

    Registering Topping Model with the Admin Site

--- Chapter_18/exercises/ex_18_4_pizzeria/pizzas/admin.py
+++ Chapter_18/exercises/ex_18_4_pizzeria/pizzas/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 from .models import Pizza
+from .models import Topping
 
 # Register your models here.
 
 admin.site.register(Pizza)
+admin.site.register(Topping)
