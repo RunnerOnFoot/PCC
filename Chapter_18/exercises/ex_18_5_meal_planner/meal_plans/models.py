e71fec6911d9c1940dffbf2b1edb78bec77c444e
Parsa-Babazadeh <parsa.babazadeh2002@gmail.com>
2023-12-30 16:25:51 +0330

    Defining models

--- Chapter_18/exercises/ex_18_5_meal_planner/meal_plans/models.py
+++ Chapter_18/exercises/ex_18_5_meal_planner/meal_plans/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Meal(models.Model):
+    """
+    An model for a single meal.
+    """
+    name = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """
+        String representation of the meal.
+        """
+        return self.name
+
+
+class MealItem(models.Model):
+    """
+    An individual part of a meal.
+    """
+    name = models.CharField(max_length=200)
+    meal = models.ForeignKey(
+        Meal, on_delete=models.CASCADE, related_name='items')
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """
+        String representation of the meal item.
+        """
+        return self.name
